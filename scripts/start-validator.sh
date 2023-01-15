b422fcc60596f19c7e627227b9a882d5aaa7275d
Alber <arblugo@gmail.com>
2023-01-15 15:45:49 +0000

    feat: update memory & validator script

--- scripts/start-validator.sh
+++ scripts/start-validator.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 exec solana-validator \
+ --identity ~/validator-keypair.json \
+ --vote-account ~/vote-account-keypair.json \
+ --rpc-port 8899 \
  --entrypoint entrypoint.devnet.solana.com:8001 \
- --entrypoint entrypoint2.devnet.solana.com:8001 \
- --entrypoint entrypoint3.devnet.solana.com:8001 \
- --entrypoint entrypoint4.devnet.solana.com:8001 \
- --entrypoint entrypoint5.devnet.solana.com:8001 \
+ --limit-ledger-size 50000000 \
+ --log ~/log/validator.log \
+ --accounts /mnt/ramdisk/solana-accounts \
+ --no-port-check
  --known-validator dv1ZAGvdsz5hHLwWXsVnM94hWf1pjbKVau1QVkaMJ92 \
  --known-validator dv2eQHeP4RFrJZ6UeiZWoc3XTtmtZCUKxxCApCDcRNV \
  --known-validator dv4ACNkpYPcE3aKmYDqZm9G5EB3J4MRoeE7WNDRBVJB \
  --known-validator dv3qDFk1DTF36Z62bNvrCXe9sKATA6xvVy6A798xxAS \
- --expected-genesis-hash EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG \
- --dynamic-port-range 8000-8020 \
- --rpc-port 8899 \
- --only-known-rpc \
- --wal-recovery-mode skip_any_corrupted_record \
- --identity ~/validator-keypair.json \
- --vote-account ~/vote-account-keypair.json \
- --log ~/log/validator.log \
- --accounts /mnt/ramdisk/solana-accounts \
- --ledger ~/ledger \
- --limit-ledger-size 50000000
+ --halt-on-known-validators-accounts-hash-mismatch
