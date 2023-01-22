2cf492d5da7c73a255ea567ab2827490161e2adb
Alber <arblugo@gmail.com>
2023-01-22 22:21:23 +0000

    fix: bash file is very picky, comments not allowed

--- scripts/start-validator.sh
+++ scripts/start-validator.sh
@@ -2,19 +2,14 @@
 exec solana-validator \
   --identity /home/sol/validator-keypair.json \
   --vote-account /home/sol/vote-account-keypair.json \
-  # Solana Labs
   --known-validator 5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on \
-  # MonkeDAO
   --known-validator dDzy5SR3AXdYWVqbDEkVFdvSPCtS9ihF5kJkHCtXoFs \
-  # Certus One
   --known-validator Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN \
-  # SerGo
   --known-validator eoKpUABi59aT4rR9HGS3LcMecfut9x7zJyodWWP43YQ \
-  # Algo|Stake
   --known-validator 9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv \
   --only-known-rpc \
-  --log ~/log/validator.log \
-  --ledger ~/ledger \
+  --log /home/sol/log/validator.log \
+  --ledger /home/sol/ledger \
   --rpc-port 8899 \
   --dynamic-port-range 8000-8020 \
   --entrypoint entrypoint.testnet.solana.com:8001 \
